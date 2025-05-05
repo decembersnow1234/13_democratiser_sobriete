5213241eeb778b828e3fe13c110907d379d10aa7
Thanh Thao Chu <strawberry-ice97@hotmail.com>
2025-05-05 17:36:36 +0200

    adding data_loader, modularizing

---
+++ stream3_visualization/Budget/config/paths.py
@@ -0,0 +1,3 @@
+DATA_DIR=C:/Users/tchu/Documents/Personnel/D4G/Viz - Carbon neutrality/13_democratiser_sobriete/stream3_visualization/Budget/Data
+OUTPUT_DIR=C:/Users/tchu/Documents/Personnel/D4G/Viz - Carbon neutrality/13_democratiser_sobriete/stream3_visualization/Budget/Output
+DATABASE_URL = postgresql://u4axloluqibskgvdikuy:g2rXgpHSbztokCbFxSyR@bk8htvifqendwt1wlzat-postgresql.services.clever-cloud.com:7327/bk8htvifqendwt1wlzat
